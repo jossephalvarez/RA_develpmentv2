0199159dfe76935f969c993b82e528f8f7109953
jalvarez <jalvarez@clever-global.com>
2019-04-29 09:39:20 +0200

    seed added with locations

--- seeders/20190428110216-demo-typeProduct.js
+++ seeders/20190428110216-demo-typeProduct.js
@@ -1,32 +1,32 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('typeProducts', [{
-          code: 'CRD',
-          name: 'Cerdo',
-          createdAt: new Date(), updatedAt: new Date()
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('typeProducts', [{
+            code: 'CRD',
+            name: 'Cerdo',
+            createdAt: new Date(), updatedAt: new Date()
 
-      }, {
-          code: 'OVN',
-          name: 'Ovino',
-          createdAt: new Date(), updatedAt: new Date()
-      },
-          {
-              code: 'BOV',
-              name: 'Bovino',
-              createdAt: new Date(), updatedAt: new Date()
-          }], {});
-  },
+        }, {
+            code: 'OVN',
+            name: 'Ovino',
+            createdAt: new Date(), updatedAt: new Date()
+        },
+            {
+                code: 'BOV',
+                name: 'Bovino',
+                createdAt: new Date(), updatedAt: new Date()
+            }], {});
+    },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-      return queryInterface.bulkDelete('typeProducts', null, {});
-  }
+          Example:
+          return queryInterface.bulkDelete('People', null, {});
+        */
+        return queryInterface.bulkDelete('typeProducts', null, {});
+    }
 };
--- seeders/20190428110229-demo-supply.js
+++ seeders/20190428110229-demo-supply.js
@@ -4,17 +4,20 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('supplies', [{
             date: '13/04/2019',
+            location_id: 1,
             createdAt: new Date(), updatedAt: new Date()
 
         },
             {
                 date: '20/04/2019',
+                location_id: 1,
                 createdAt: new Date(), updatedAt: new Date()
 
 
             },
             {
                 date: '13/04/2019',
+                location_id: 2,
                 createdAt: new Date(), updatedAt: new Date()
             }], {});
     },
